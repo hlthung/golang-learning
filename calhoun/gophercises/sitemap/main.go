93c54139d3ba8cef326d2643d9ae34434a4e4e7a
hlthung <adelinethung712@gmail.com>
2022-09-17 16:14:43 +0800

    Sitemap parsing and building links

---
+++ calhoun/gophercises/sitemap/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/hlthung/golang-learning/calhoun/gophercises/link"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
+	flag.Parse()
+
+	fmt.Println(*urlFlag)
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	links, _ := link.Parse(resp.Body)
+	for _, l := range links {
+		fmt.Println(l)
+	}
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
+}
