b28c32e8b83c335b260ee11b213bdab5762d9d5a
hlthung <adelinethung712@gmail.com>
2022-09-18 22:43:39 +0800

    Implementing a bfs

--- calhoun/gophercises/sitemap/main.go
+++ calhoun/gophercises/sitemap/main.go
@@ -10,21 +10,90 @@ import (
 	"strings"
 )
 
+/*
+   1. GET the webpage
+   2. parse all the links on the page
+   3. build proper urls with our links
+   4. filter out any links w/ a diff domain
+   5. Find all pages (BFS)
+   6. print out XML
+*/
+
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
+	maxDepth := flag.Int("depth", 3, "the maximum number of links deep to traverse")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
-	pages := get(*urlFlag)
+	pages := bfs(*urlFlag, *maxDepth)
 	for _, page := range pages {
 		fmt.Println(page)
 	}
 }
 
+type empty struct{}
+
+func bfs_empty(urlStr string, maxDepth int) []string {
+	seen := make(map[string]empty) // we dont care about the value so put struct{} as this type has the minimum data size / memory
+	var q map[string]empty
+	nq := map[string]empty{
+		urlStr: {},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]empty)
+		if len(q) == 0 {
+			break
+		}
+		for url, _ := range q {
+			// tell me if there's a url key inside the seen map
+			if _, ok := seen[url]; ok {
+				continue // if seen continue
+			}
+			seen[url] = empty{} // mark this url as seen
+			for _, link := range get(url) {
+				nq[link] = empty{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen)) //create with seen size to optimise
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+
+func bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{}) // we dont care about the value so put struct{} as this type has the minimum data size / memory
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: {},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		if len(q) == 0 {
+			break
+		}
+		for url, _ := range q {
+			// tell me if there's a url key inside the seen map
+			if _, ok := seen[url]; ok {
+				continue // if seen continue
+			}
+			seen[url] = struct{}{} // mark this url as seen
+			for _, link := range get(url) {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen)) //create with seen size to optimise
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+	return ret
+}
+
 func get(urlStr string) []string {
 	resp, err := http.Get(urlStr)
 	if err != nil {
-		panic(err)
+		return []string{}
 	}
 	defer resp.Body.Close()
 
