72502e2411a2225a834fe101b3f304e5557ee960
hlthung <adelinethung712@gmail.com>
2022-09-25 21:20:25 +0800

    Add comment

--- calhoun/gophercises/task/main.go
+++ calhoun/gophercises/task/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// According to Calhoun, Go is awesome for building CLI that distribute to different OS,
+// As Go simply converts / compiles the code into a binary file for any given platform
+// while Java needs the JVM to interpret compiled code. However, below articles does mention
+// the limitation of Go and did a comparison between Java and Go.
+// https://spiralscout.com/blog/when-to-use-go-vs-java-one-programmers-take-on-two-top-languages
+// https://www.turing.com/blog/golang-vs-java-which-language-is-best/
+// Note: This behavior actually made Go faster than Java on almost every benchmark.
+// This is due to how it is compiled: Go doesn’t rely on a virtual machine to compile its code.
+// It gets compiled directly into a binary file. Because Go does not have the VM, it is faster.
+// But that that VM also helps Java work on more platforms
 func main() {
 	fmt.Println("TODO")
 }
