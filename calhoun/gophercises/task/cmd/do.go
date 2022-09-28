de939b1f2170f68ffe36ab64fc40f9af9b61e653
hlthung <adelinethung712@gmail.com>
2022-09-28 22:53:33 +0800

    Add do and list

--- calhoun/gophercises/task/cmd/do.go
+++ calhoun/gophercises/task/cmd/do.go
@@ -1,1 +1,14 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var doCmd = &cobra.Command{
+	Use:   "do",
+	Short: "Marks a task as complete",
+}
+
+func init() {
+	RootCmd.AddCommand(doCmd)
+}
--- calhoun/gophercises/task/cmd/list.go
+++ calhoun/gophercises/task/cmd/list.go
@@ -1,1 +1,14 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all of your tasks.",
+}
+
+func init() {
+	RootCmd.AddCommand(listCmd)
+}
