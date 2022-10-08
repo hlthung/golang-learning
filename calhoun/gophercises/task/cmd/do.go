d0a676f228ecf963882ceb1c0e753f932d5a73bb
hlthung <adelinethung712@gmail.com>
2022-10-08 22:52:22 +0800

    Finalizing all cmd

--- calhoun/gophercises/task/cmd/do.go
+++ calhoun/gophercises/task/cmd/do.go
@@ -1,12 +1,44 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/hlthung/golang-learning/calhoun/gophercises/task/db"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Marks a task as complete",
+	Run: func(cmd *cobra.Command, args []string) {
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err)
+			return
+		}
+		for _, id := range ids {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%d\" as completed. Error: %s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\" as completed.\n", id)
+			}
+		}
+	},
 }
 
 func init() {
